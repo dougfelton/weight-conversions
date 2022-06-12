6f84f1a92a064acf7b3ae633ae6ddeb95dcb8fb5
Doug Felton <dougfelton@dougfelton.com>
2022-06-12 15:50:45 -0400

    Add toStones

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -7,3 +7,9 @@ it('can convert kilograms to pounds', function () {
 
     expect($pounds)->toEqual(220.46);
 });
+
+it('can convert kilograms to stones', function () {
+    $stones = Weight::kilograms(100)->toStones();
+
+    expect($stones)->toEqual(15.747);
+});
